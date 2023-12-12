5123b7b16572844088642163e55f19a777de44c2
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-12-12 11:29:32 +0100

    Sample solution for day 5

--- src/main/java/de/workshops/bookshelf/book/Book.java
+++ src/main/java/de/workshops/bookshelf/book/Book.java
@@ -1,9 +1,28 @@
 package de.workshops.bookshelf.book;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
 class Book {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @JsonIgnore
+  private Long id;
 
   private String title;
 
@@ -12,4 +31,21 @@ class Book {
   private String author;
 
   private String isbn;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+      return false;
+    }
+    Book book = (Book) o;
+    return id != null && Objects.equals(id, book.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
 }
